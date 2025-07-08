da2cf219187a8a2e188c679340b2fa69de1245a9
sergio <sergio@cimarron.io>
2025-07-08 08:45:23 -0700

    Initial Commit

---
+++ brainstorm/brainstormApp.swift
@@ -0,0 +1,32 @@
+//
+//  brainstormApp.swift
+//  brainstorm
+//
+//  Created by Sergio Sanchez on 7/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct brainstormApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ brainstormTests/brainstormTests.swift
@@ -0,0 +1,16 @@
+//
+//  brainstormTests.swift
+//  brainstormTests
+//
+//  Created by Sergio Sanchez on 7/8/25.
+//
+
+import Testing
+
+struct brainstormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
